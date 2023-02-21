e7db64d3925a5afdb4cc3692aac8940e17d0b969
combateer3 <scottcorbat@yahoo.com>
2023-02-20 21:00:31 -0500

    Function to create basic file structure New argument format for source and destination directories

--- file_utils.py
+++ file_utils.py
@@ -1,3 +1,17 @@
+from datetime import datetime
+import os
+
+
+def generate_file_structure(file_data: datetime, dest_dir: str) -> None:
+    year = file_data.year
+    month = file_data.month
+    day = file_data.day
+
+    path = f"{year}/{month}/{day}"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
 class FileData:
     def __init__(self) -> None:
         pass
--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-import os, shutil
+import os, sys
 from datetime import datetime, timezone, timedelta
 
 from constants import *
+import file_utils
 
 
-def main():
-    full_path = f"{os.getcwd()}/{DEFAULT_FOLDER}"
-    os.chdir(full_path)
+def main(argv):
+    if len(argv) < 2:
+        print("This script expects two arguments:")
+        print("Format: python main.py <source_directory> <destination_directory>")
+        sys.exit()
+
+    source_dir = argv[0]
+    dest_dir = argv[1]
+    if not source_dir:
+        print("An invalid source directory was provided!")
+    if not dest_dir:
+        print("An invalid destination directory was provided!")
+
+    # set given directory
+    os.chdir(source_dir)
     # get file statistics
-    result = os.stat("GX010126.mp4")
+    result = os.stat("R:\GoPro Auto\drop\GX010126.mp4")
     print(result)
     # creates a datetime object for EST
     modified = datetime.fromtimestamp(result.st_mtime, tz=timezone(timedelta(hours=EST_HOURS_OFFSET)))
-    print(modified)
+
+    # create file structure
+    file_utils.generate_file_structure(modified, dest_dir)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
